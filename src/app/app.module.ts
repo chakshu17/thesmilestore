2fcc6f2eb9298c86a0f05b9e32a03077483c38aa
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-06-11 11:51:47 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EnquirybuttonComponent } from './enquirybutton/enquirybutton.component';
+import { CrownandbridgesComponent } from './treatment/crownandbridges/crownandbridges.component';
+import { RootcanalComponent } from './treatment/rootcanal/rootcanal.component';
+import { PeriodonticsComponent } from './treatment/periodontics/periodontics.component';
+import { PediatricdentistryComponent } from './treatment/pediatricdentistry/pediatricdentistry.component';
+import { OrthodonticsComponent } from './treatment/orthodontics/orthodontics.component';
+import { ImpactedteethComponent } from './treatment/impactedteeth/impactedteeth.component';
+import { PatienteducationComponent } from './treatment/patienteducation/patienteducation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EnquirybuttonComponent,
+    CrownandbridgesComponent,
+    RootcanalComponent,
+    PeriodonticsComponent,
+    PediatricdentistryComponent,
+    OrthodonticsComponent,
+    ImpactedteethComponent,
+    PatienteducationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
